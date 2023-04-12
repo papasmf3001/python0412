5a7c6c328d2cdfc965f21e837ea13fb72f3d5495
papasmf3001 <papasmf3001@gmail.com>
2023-04-12 11:56:35 +0900

    서식 지정 연습1

---
+++ DemoFormat.py
@@ -0,0 +1,22 @@
+# DemoFormat.py 
+url = "http://wwww.credu.com/?page=" + str(1)  
+print(url)
+
+for x in range(1,6):
+    print(x,"*",x,"=",x*x)
+
+print("---오른쪽정렬---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).rjust(3))
+
+print("---0으로 채우기---")
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x).zfill(5)) 
+
+
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(15000000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
