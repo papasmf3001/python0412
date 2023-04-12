d7850b4f88e1f12cb076493a148e61a2f6810a31
papasmf3001 <papasmf3001@gmail.com>
2023-04-12 12:18:50 +0900

    파일 연습 완료

--- DemoFormat.py
+++ DemoFormat.py
@@ -20,3 +20,23 @@ print("{0:,}".format(15000000))
 print("{0:e}".format(4/3))
 print("{0:f}".format(4/3))
 print("{0:.2f}".format(4/3))
+
+#raw string notation:특수문자로 가공하지 않은 것 
+f = open(r"c:\work\demo.txt", "wt", encoding="utf-8")
+f.write("첫번째라인\n두번째라인\n세번째라인\n")
+f.close() 
+print( f.closed )
+
+#파일을 읽기 
+f = open(r"c:\work\demo.txt", "rt", encoding="utf-8")
+result = f.read()
+print("---readline()---")
+f.seek(0)
+print( f.readline(), end="" )
+print( f.readline(), end="" )
+f.seek(0)
+lst  = f.readlines() 
+print( lst )
+
+f.close()
+print( result )
